dc0fb3bcd096cedd2f2e4cd26ea5c35fdc6badfe
seikichi <seikichi@mai.localdomain>
2010-06-30 18:42:54 +0900

    add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,15 @@ setup(name='tuitwi',
       long_description="""\
       TUI twiter client(using curses).
 """,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console :: Curses',
+          'Natural Language :: Japanese',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Topic :: Communications'
+          ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='curses tui twitter',
       author='seikichi',
       author_email='seikichi@kmc.gr.jp',
